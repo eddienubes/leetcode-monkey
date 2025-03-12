2bf89b179265fdd6acfe1b81ada97afa4d09dada
eddienubes <cool.gegeg@gmail.com>
2025-03-13 00:23:33 +0100

    fix: migrations

---
+++ scripts/copy.ts
@@ -0,0 +1,14 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+export const copy = async (): Promise<void> => {
+  const servicePath = path.join(__dirname, '..')
+
+  await fs.cp(
+    path.join(servicePath, 'src/pg/migrations'),
+    'build/pg/migrations',
+    { recursive: true },
+  )
+}
+
+void copy()
