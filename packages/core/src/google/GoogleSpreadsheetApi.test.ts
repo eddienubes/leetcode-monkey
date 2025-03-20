4f4c2697c38a7cedd7d806f3227fc319e3cbf1cf
eddienubes <cool.gegeg@gmail.com>
2025-03-20 23:50:55 +0100

    feat: spreadsheets sync wip

--- packages/core/src/google/GoogleSpreadsheetApi.test.ts
+++ packages/core/src/google/GoogleSpreadsheetApi.test.ts
@@ -2,8 +2,7 @@ import { GoogleAuthService } from '@/google/GoogleAuthService'
 import { GoogleSpreadsheetApi } from '@/google/GoogleSpreadsheetApi'
 
 describe('GoogleSpreadsheetApi', () => {
-  const token =
-    'ya29.a0AeXRPp4NROXEfOC3jzealye8ZkgIUKMmfcohfvBltKcnrScRP0oPkmHx6x0IvUin2WJHp7EZq3U2tezUokIk6iuYjzZCtfIJYuoBI2pTyS_seslMvGMH-SwbdLaVUW8bmwLCF5hM3D6G5dT62XFHtFrD8zvzpWHG-CUljpShaCgYKAR0SARASFQHGX2MiKRedSbwms1L-adfQ8CAJqQ0175'
+  const token = ''
 
   const auth = new GoogleAuthService()
   const sheets = new GoogleSpreadsheetApi(auth.getClient(token))
